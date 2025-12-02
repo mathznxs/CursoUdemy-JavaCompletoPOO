10abf8d01516dabef50b2cda27ec436853d6c558
mathznxs <math945374081@gmail.com>
2025-12-02 09:08:43 -0300

    Estrutura Repetitiva For

--- src/EstruturasRepetitivas/Fundamentos/EstruturaFor1.java
+++ src/EstruturasRepetitivas/Fundamentos/EstruturaFor1.java
@@ -1,4 +1,22 @@
 package EstruturasRepetitivas.Fundamentos;
 
+import java.util.Scanner;
+
 public class EstruturaFor1 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de números que deseja somar: ");
+        int n = sc.nextInt();
+        int soma = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite um número: ");
+            int x = sc.nextInt();
+            soma += x;
+        }
+
+        System.out.printf("\nSoma: %d", soma);
+
+    }
 }
