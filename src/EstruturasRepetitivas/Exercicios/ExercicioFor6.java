3ee9e1ff5d931e70838c3190a80a903a65e183a7
mathznxs <math945374081@gmail.com>
2025-12-04 10:47:57 -0300

    Exercício For 6

--- src/EstruturasRepetitivas/Exercicios/ExercicioFor6.java
+++ src/EstruturasRepetitivas/Exercicios/ExercicioFor6.java
@@ -1,14 +1,24 @@
 package EstruturasRepetitivas.Exercicios;
 
 /*
-
- */
+    Ler um número inteiro N e calcular todos os seus divisores.
+*/
 
 import java.util.Scanner;
 
 public class ExercicioFor6 {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número e descubra quais são seus divisores: ");
+        int num = sc.nextInt();
+
+        System.out.print("\nDivisores: ");
+        for (int i = 1; i <= num; i++) {
+            if ( num % i == 0 ) {
+                System.out.printf("%d ", i);
+            }
+        }
 
         sc.close();
     }
