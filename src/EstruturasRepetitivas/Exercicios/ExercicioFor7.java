d2eb45daa5f65f803c673f5c5ed375e66bdf0b04
mathznxs <math945374081@gmail.com>
2025-12-04 10:56:14 -0300

    Exercício For 7

--- src/EstruturasRepetitivas/Exercicios/ExercicioFor7.java
+++ src/EstruturasRepetitivas/Exercicios/ExercicioFor7.java
@@ -1,14 +1,24 @@
 package EstruturasRepetitivas.Exercicios;
 
 /*
-
- */
+    Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+    começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+    exemplo
+*/
 
 import java.util.Scanner;
 
 public class ExercicioFor7 {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int num = sc.nextInt();
+
+        System.out.println();
+        for (int i = 1; i <= num; i++) {
+            System.out.println(i + " " + (i * i) + " " + (i * i * i));
+        }
 
         sc.close();
     }
