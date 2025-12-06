a1e9eca656a3dca1895108e6b866ea482fc3bb19
mathznxs <math945374081@gmail.com>
2025-12-05 23:47:15 -0300

    Tópicos Básicos em Java - Operação por Split

--- src/TopicosBasicosJava/Fundamentos/FuncoesString.java
+++ src/TopicosBasicosJava/Fundamentos/FuncoesString.java
@@ -1,6 +1,6 @@
 package TopicosBasicosJava.Fundamentos;
 
-public class FuncoesString1 {
+public class FuncoesString {
     public static void main (String[] args) {
         String original = "abcde FGHIJ ABC abc DEFG   ";
         String s01 = original.toLowerCase();
---
+++ src/TopicosBasicosJava/Fundamentos/OperacaoSplit.java
@@ -0,0 +1,12 @@
+package TopicosBasicosJava.Fundamentos;
+
+public class OperacaoSplit {
+    public static void main (String[] args) {
+        String s = "potato apple lemon";
+
+        String[] vect = s.split(" ");
+        System.out.println(vect[0]);
+        System.out.println(vect[1]);
+        System.out.println(vect[2]);
+    }
+}
