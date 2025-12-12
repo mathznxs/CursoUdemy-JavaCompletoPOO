8588938bedb90d999a2a58dd8c446c2b73ede5c9
mathznxs <math945374081@gmail.com>
2025-12-11 22:43:45 -0300

    Resolvendo problema 2 usando POO - sistema de estoque

--- src/com/mathdev/javacourse/poo/problema2/application/Program.java
+++ src/com/mathdev/javacourse/poo/problema2/application/Program.java
@@ -1,4 +1,38 @@
 package com.mathdev.javacourse.poo.problema2.application;
 
+import com.mathdev.javacourse.poo.problema2.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main (String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data: \n");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println("Product data: " + product);
+
+        System.out.print("\nEnter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.print("\nUpdated data: " + product);
+
+        System.out.print("\nEnter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.print("\nUpdated data: " + product);
+
+        sc.close();
+    }
 }
