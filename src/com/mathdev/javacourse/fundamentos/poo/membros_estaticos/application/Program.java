d97b7ece503a2a67d6a0ceeb9d7a22127d699850
mathznxs <math945374081@gmail.com>
2025-12-24 08:03:24 -0300

    Membros estáticos - Versão 2.0

--- src/com/mathdev/javacourse/fundamentos/poo/membros_estaticos/application/Program.java
+++ src/com/mathdev/javacourse/fundamentos/poo/membros_estaticos/application/Program.java
@@ -1,33 +1,26 @@
 package com.mathdev.javacourse.fundamentos.poo.membros_estaticos.application;
 
+import com.mathdev.javacourse.fundamentos.poo.membros_estaticos.util.Calculator;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-
-    public static double PI = 3.14;
-
     public static void main (String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = circumference(radius);
-        double v = volume(radius);
+        double c = Calculator.circumference(radius);
+        double v = Calculator.volume(radius);
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI Value: %.2f%n", PI);
+        System.out.printf("PI Value: %.2f%n", Calculator.PI);
 
         sc.close();
     }
-    public static double circumference(double radius) {
-        return 2.0 * PI * radius;
-    }
-    public static double volume(double radius) {
-        return 4.0 * PI * Math.pow(radius, 3) / 3.0;
-    }
+
 }
--- src/com/mathdev/javacourse/fundamentos/poo/membros_estaticos/util/Calculator.java
+++ src/com/mathdev/javacourse/fundamentos/poo/membros_estaticos/util/Calculator.java
@@ -1,5 +1,11 @@
 package com.mathdev.javacourse.fundamentos.poo.membros_estaticos.util;
 
 public class Calculator {
-
+    public static final double PI = 3.14;
+    public static double circumference(double radius) {
+        return 2.0 * PI * radius;
+    }
+    public static double volume(double radius) {
+        return 4.0 * PI * Math.pow(radius, 3) / 3.0;
+    }
 }
